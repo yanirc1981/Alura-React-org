eca5e061f3252d5f7d956401a08ac7828e59067f
yanirc1981 <cerzurc@hotmail.com>
2023-08-25 17:14:59 -0300

    add changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './components/Header/Header';
+import Form from './components/Form/Form';
 
 function App() {
   return (
-    <div className="App">
+    <div >
    
     <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hola mundo!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendiendo React
-        </a>
-      </header>
+    <Form />  
     </div>
   );
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,12 @@
+import './Form.css';
+
+const Form = () => {
+
+    return <section className='form'>
+        <form>
+            <h2>Completa el formulario para crear el colaborador.</h2>
+        </form>
+    </section>
+}
+
+export default Form 
