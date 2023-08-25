e8a54e3da6bba688b7991c4fede63005e82758d8
yanirc1981 <cerzurc@hotmail.com>
2023-08-25 15:37:40 -0300

    se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+   
+    <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hola mundo!!!
         </p>
         <a
           className="App-link"
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendiendo React
         </a>
       </header>
     </div>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css';
+
+function Header(){
+    return <header className='header'>
+        <img src='/img/header.png' alt='banner' />
+    </header>
+   
+}
+
+export default Header;
